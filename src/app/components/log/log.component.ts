df6481405081ab4cc8aba88826062991a84f6a0d
Pair <pair@pivotal.io>
2018-02-11 13:49:53 +0800

    Add links between both pages

--- src/app/components/log/log.component.ts
+++ src/app/components/log/log.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Update } from '../../models/update';
 import { AngularFirestore } from 'angularfire2/firestore';
 
